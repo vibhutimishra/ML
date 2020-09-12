0d9a67db7703cb4404a107922a01ca94f812a19e
vibhutimishra <vibhuti.mishra09@gmail.com>
2020-09-12 13:07:30 +0530

    changes

--- gradient-descent.py
+++ gradient-descent.py
@@ -2,8 +2,6 @@
 # coding: utf-8
 
 # In[4]:
-
-
 import pandas as pd
 import numpy as np
 from pandas import DataFrame
@@ -12,25 +10,15 @@ from mpl_toolkits.mplot3d import Axes3D
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 
-
-# In[5]:
-
-
+#function
 def cubic(x):
     return x**3+x**2+x+1
 def dfx(x):
     return 3*x*x+2*x+1
 
-
-# In[6]:
-
-
 x_=np.linspace(start=-2,stop=3,num=100)
 
-
-# In[10]:
-
-
+#function plot
 plt.figure(figsize=[10,5])
 plt.subplot(1,2,1)
 plt.title('function')
@@ -42,16 +30,8 @@ plt.title('slope')
 plt.grid()
 plt.plot(x_,dfx(x_))
 
-
-# In[8]:
-
-
 plt.plot(x_,dfx(x_))
-
-
 # In[9]:
-
-
 newx=2
 prev=0
 cons=0.001
@@ -67,14 +47,10 @@ print("minimum at ",newx)
 print("slope of min ",dfx(newx))
 print("value of function", cubic(newx))
 xlist=np.array(xlist)
-
-
 # # Example 2- Multiple minima vs initial guess and advanced function
 # 
 # # $$g(x)=x^4-4x^2+5$$
 # 
-
-# In[11]:
 
 
 x_2 = np.linspace(start=-2,stop=2,num=1000)
@@ -84,10 +60,7 @@ def fun(x):
 
 def dfx2(x):
     return 4*x**3-8*x
-
-
 # In[12]:
-
 
 plt.figure(figsize=[20,5])
 plt.xlim(-2,2)
@@ -103,13 +76,9 @@ plt.ylim(-6,8)
 plt.title('slope')
 plt.grid()
 plt.plot(x_2,dfx2(x_2))
-
-
 # # gradient descent as python function when we have 2 local minima
 
 # In[13]:
-
-
 def gradient_descent(derivative_fun,initial_value,const=0.02,prec=0.001):
     newx=initial_value
     n=0
@@ -129,8 +98,6 @@ def gradient_descent(derivative_fun,initial_value,const=0.02,prec=0.001):
     return np.array(xlist),n
 xlist,n=gradient_descent(dfx2,0)
 print("after steps",n)
-
-
 # In[14]:
 
 
@@ -166,13 +133,6 @@ def h(x):
 def dh(x):
     return 5*x**4-8*x**3
 
-
-# In[ ]:
-
-
-
-
-
 # In[16]:
 
 
@@ -195,8 +155,6 @@ def gradient_descent(derivative_fun,initial_value,const=0.02,prec=0.001):
     return np.array(xlist),n
 xlist,n=gradient_descent(dh,0.2)
 print("after steps",n)
-
-
 # In[17]:
 
 
@@ -216,10 +174,3 @@ plt.title('slope')
 plt.grid()
 plt.plot(x_3,dh(x_3))
 plt.scatter(xlist,dh(xlist),color='red')
-
-
-# In[ ]:
-
-
-
-
